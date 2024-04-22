0756ebf21c91f2a23403fd735a687a3e627b2b45
Ian Ubaldo <ianzachary.u@gmail.com>
2024-04-21 22:40:54 -0500

    updated mod to 1.19.7, refactored gear sound behavior

--- gearofmadness/src/BehaviorGearSound.cs
+++ gearofmadness/src/BehaviorGearSound.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
+using ProtoBuf;
+
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
-using Vintagestory.API.MathTools;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.Client;
 using Vintagestory.API.Server;
@@ -10,34 +11,34 @@ using Vintagestory.API.Server;
 namespace gearofmadness.src
 {
     public class BehaviorGearSound : EntityBehavior
-    {
-        ICoreClientAPI capi;
-        ILoadedSound gearSound;
-        bool requireInitSounds;
+    {       
         bool enabled = true; // if temporal stability isn't enabled, this behavior does nothing
-        bool isSelf;
-        float oneSecAccum = 0f;
+        int tickInterval = 20000;
+
 
         public override string PropertyName() { return "gearsoundaffected"; }
 
+
         public BehaviorGearSound(Entity entity) : base(entity) { }
+
 
         public override void Initialize(EntityProperties properties, JsonObject attributes)
         {
             base.Initialize(properties, attributes);
 
-            if (entity.Api.Side == EnumAppSide.Client)
-            {
-                capi = entity.Api as ICoreClientAPI;
-                requireInitSounds = true;
-            }
-
             // check if the world is using temporal stability
             enabled = entity.Api.World.Config.GetBool("temporalStability", true);
 
+            if (!enabled)
+                return;
+
             if (!entity.WatchedAttributes.HasAttribute("temporalStability"))
                 OwnStability = 1;
+
+            if (entity.Api.Side == EnumAppSide.Server)
+                GearOfMadnessMain.sapi.Event.RegisterGameTickListener(new Action<float>(OnGameTick), tickInterval);
         }
+
 
         public double OwnStability
         {
@@ -45,78 +46,22 @@ namespace gearofmadness.src
             set { entity.WatchedAttributes.SetDouble("temporalStability", value); }
         }
 
+
         public override void OnGameTick(float deltaTime)
         {
-            // sanity checks, don't want to do stuff on tick if we don't have to
             if (!enabled)
                 return;
 
-            if (requireInitSounds)
-            {
-                capi = entity.Api as ICoreClientAPI;
+            if (entity.Api.Side == EnumAppSide.Client)
+                return;
 
-                if (capi == null)
-                    return;
+            GearSoundMessage message = 
+                new() { 
+                    shouldPlay = entity.WatchedAttributes.HasAttribute("temporalStability") && OwnStability <= 0.75,
+                    stability = (float)OwnStability 
+                };
 
-                isSelf = capi.World.Player.Entity.EntityId == entity.EntityId;
-                if (!isSelf)
-                    return;
-
-                gearSound = capi.World.LoadSound(new SoundParams()
-                {
-                    Location = new AssetLocation("gearofmadness", "sounds/bs2_gear_loop.ogg"),
-                    ShouldLoop = true,
-                    RelativePosition = true,
-                    DisposeOnFinish = false,
-                    SoundType = EnumSoundType.SoundGlitchunaffected,
-                    Volume = 0f
-                });
-
-                requireInitSounds = false;
-            }
-
-            EntityPlayer entityPlayer = entity as EntityPlayer;
-            IPlayer player = (entityPlayer != null) ? entityPlayer.Player : null;
-            IServerPlayer serverPlayer = player as IServerPlayer;
-            if (entity.World.Side == EnumAppSide.Client)
-            {
-                if (!(entity.World.Api as ICoreClientAPI).PlayerReadyFired)
-                    return;
-            }
-            else
-            {
-                serverPlayer = entity.World.PlayerByUid(((EntityPlayer)entity).PlayerUID) as IServerPlayer;
-                if (serverPlayer != null && serverPlayer.ConnectionState != EnumClientState.Playing)
-                    return;
-            }
-
-            deltaTime = GameMath.Min(0.5f, deltaTime);
-            oneSecAccum += deltaTime; // do every second
-            if (oneSecAccum > 1)
-            {
-                oneSecAccum = 0;
-
-                if (!isSelf)
-                    return;
-
-                float fadeSpeed = 3f;
-                if (gearSound != null)
-                {
-                    if (entity.WatchedAttributes.HasAttribute("temporalStability"))
-                    {
-                        if (OwnStability <= 0.75) // start playing sound, volume scaling with how low stability is
-                        {
-                            if (!gearSound.IsPlaying)
-                                gearSound.Start();
-
-                            float volume = (0.4f - (float)OwnStability) * 1 / 0.4f;
-                            gearSound.FadeTo(Math.Min(1, volume), 0.95f * fadeSpeed, (s) => { });
-                        }
-                    }
-                    else
-                        gearSound.FadeTo(0, 0.95f * fadeSpeed, (s) => { gearSound.Stop(); });
-                }
-            }
+            GearOfMadnessMain.sSoundChannel.SendPacket(message, (IServerPlayer)((EntityPlayer)entity).Player);
         }
     }
 }
--- gearofmadness/src/GearOfMadnessMain.cs
+++ gearofmadness/src/GearOfMadnessMain.cs
@@ -1,37 +1,183 @@
-﻿using System;
-
+﻿using ProtoBuf;
+using System;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Server;
 
 namespace gearofmadness.src
 {
     public class GearOfMadnessMain : ModSystem
     {
+        internal static readonly bool debugMode = false;
+
         const string CONFIG_FILE_NAME = "gearofmadnessconfig.json";
         internal static GMConfig config;
+
+        internal static ICoreServerAPI sapi;
+
+        internal static IServerNetworkChannel sSoundChannel;
+
+        ICoreClientAPI capi;
+        ILoadedSound gearSound;
+
+        public override void StartPre(ICoreAPI api)
+        {
+            base.StartPre(api);
+
+            try
+            {
+                config = api.LoadModConfig<GMConfig>(CONFIG_FILE_NAME);
+            }
+            catch (Exception) { }
+
+            if (config == null)
+                config = new();
+
+            api.StoreModConfig(config, CONFIG_FILE_NAME);
+        }
 
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
             api.RegisterItemClass("itemgearofmadness", typeof(ItemGearOfMadness));
             api.RegisterEntityBehaviorClass("gearsoundaffected", typeof(BehaviorGearSound));
+
+
+            api.Event.OnEntitySpawn += AddEntityBehaviors;
+            api.Event.OnEntityLoaded += AddEntityBehaviors;
         }
 
-        public override void StartServerSide(ICoreServerAPI sapi)
+
+        public override void StartServerSide(ICoreServerAPI api)
         {
-            base.StartServerSide(sapi);
+            base.StartServerSide(api);
 
-            try
-            {
-                config = sapi.LoadModConfig<GMConfig>(CONFIG_FILE_NAME);
-            } catch (Exception) { }
+            sapi = api;
+            sSoundChannel =
+                sapi.Network.RegisterChannel("gearSoundChannel")
+                .RegisterMessageType<GearSoundMessage>();
 
-            if (config == null)
-                config = new();
-
-            sapi.StoreModConfig(config, CONFIG_FILE_NAME);
-
-            
+            if (debugMode)
+                RegisterCommands(sapi);
         }
+
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(capi);
+
+            capi = api;
+
+            capi.Network.RegisterChannel("gearSoundChannel")
+                .RegisterMessageType<GearSoundMessage>()
+                .SetMessageHandler<GearSoundMessage>(OnPlaySound);
+        }
+
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            base.AssetsFinalize(api);
+
+            api.World.Config.SetBool("GearOfMadness_ItemGearOfMadness_IsEnabled", config.enableGearItem);
+
+            if (api.Side == EnumAppSide.Client)
+                InitSounds();
+        }
+
+
+        public void AddEntityBehaviors(Entity targetEntity)
+        {
+            if (targetEntity == null || targetEntity is not EntityPlayer)
+                return;
+
+            if (config.enableGearSound)
+                targetEntity.AddBehavior(new BehaviorGearSound(targetEntity));
+        }
+
+
+        void RegisterCommands(ICoreServerAPI sapi)
+        {
+            CommandArgumentParsers parsers = sapi.ChatCommands.Parsers;
+
+            sapi.ChatCommands
+                .GetOrCreate("gom")
+                .IgnoreAdditionalArgs()
+                .RequiresPrivilege("worldedit")
+                .WithDescription("Gear of Madness Mod debug commands.")
+
+                .BeginSubCommand("setStability")
+                    .WithDescription("Sets the temporal stability of the calling player.")
+                    .WithArgs(parsers.DoubleRange("newStability", 0, 1))
+                    .HandleWith(OnCmdSetStability)
+                .EndSubCommand()
+            ;
+        }
+
+
+        TextCommandResult OnCmdSetStability(TextCommandCallingArgs args)
+        {
+            double oldStability = args.Caller.Player.Entity.WatchedAttributes.GetDouble("temporalStability");
+            double newStability = Convert.ToDouble(args[0]);
+            
+            args.Caller.Player.Entity.WatchedAttributes.SetDouble("temporalStability", newStability);
+
+            Mod.Logger.Notification("Stability set from " + oldStability + " -> " + newStability);
+
+            return TextCommandResult.Success();
+        }
+
+
+        void OnPlaySound(GearSoundMessage message)
+        {
+            float fadeSpeed = 3f;
+
+            if (gearSound == null)
+                return;
+
+            if (message.shouldPlay) {
+                if (debugMode)
+                    Mod.Logger.Notification("playing gear sound");
+
+                if (!gearSound.IsPlaying)
+                    gearSound.Start();
+
+                float volume = (0.4f - message.stability) * 1 / 0.4f; // volume scaling with how low stability is
+                gearSound.FadeTo(Math.Min(1, volume), 0.95f * fadeSpeed, (s) => { });
+            }
+            else
+            {
+                if (debugMode)
+                    Mod.Logger.Notification("stopping gear sound");
+
+                gearSound.FadeTo(0, 0.95f * fadeSpeed, (s) => { gearSound.Stop(); });
+            }
+        }
+
+
+        void InitSounds()
+        {
+            if (debugMode)
+                Mod.Logger.Notification("initializing gear sound");
+
+            gearSound = capi.World.LoadSound(new SoundParams()
+            {
+                Location = new AssetLocation("gearofmadness", "sounds/bs2_gear_loop.ogg"),
+                ShouldLoop = true,
+                RelativePosition = true,
+                DisposeOnFinish = false,
+                SoundType = EnumSoundType.SoundGlitchunaffected,
+                Volume = 0f
+            });
+        }
+    }
+
+
+    // tells the client to play a sound
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class GearSoundMessage
+    {
+        public bool shouldPlay;
+        public float stability;
     }
 }
